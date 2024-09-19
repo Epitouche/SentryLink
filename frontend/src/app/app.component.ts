4810dcebc72e7c098f3b93efa488ee8611ce9f8b
OxiiLB <romain.giraud@epitech.eu>
2024-09-19 16:24:19 +0000

    [UPD] animation toggle bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  isOpen = true;
+
   title = 'frontend';
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
