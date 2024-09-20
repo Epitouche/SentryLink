8cc8abfe8285e60d8a2c144c6d579b55c21131f1
arthurgauffre <arth.gauffre@gmail.com>
2024-09-20 14:21:14 +0200

    [ADD] mind-map archi

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { MindMapComponent } from './mind-map/mind-map.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'mind-map', component: MindMapComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
