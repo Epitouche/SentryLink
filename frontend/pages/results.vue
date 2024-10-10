9e9226c945d917b3aeb7cf30269331cc01ef83ed
Alea Chlodnik <achlodnik@gmail.com>
2024-10-10 10:53:15 +0200

    [UPD] results.vue
    creating outline for page. Now putting on pause to do project page first

--- frontend/pages/results.vue
+++ frontend/pages/results.vue
@@ -1,13 +1,13 @@
 <!-- Use skeleten compononet from nuxt for when page is loading -->
 <template>
-  <UContainer class="bg-red-400 w-full p-2 flex flex-row justify-between">
+  <UContainer class="max-w-full p-2 flex flex-row justify-between">
      <div>
-      <UButton><Icon name="bytesize:arrow-left" /></UButton>
+      <UButton color="black" variant="link"><Icon name="bytesize:arrow-left"/></UButton>
      </div>
-     <div>
-      <UButton><Icon name="bytesize:paperclip" /></UButton>
-      <UButton><Icon name="bytesize:calendar" /></UButton>
-      <UButton><Icon name="bytesize:ellipsis-vertical" /></UButton>
+     <div class="flex gap-3">
+      <UButton color="black" variant="link"><Icon name="bytesize:paperclip" /></UButton>
+      <UButton color="black" variant="link"><Icon name="bytesize:calendar" /></UButton>
+      <UButton color="black" variant="link"><Icon name="bytesize:ellipsis-vertical" /></UButton>
      </div>
   </UContainer>
   <UContainer>
