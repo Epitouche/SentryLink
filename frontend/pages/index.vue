c1a2d907506ba88e6e1daef355c6b2c43a3d9c4e
OxiiLB <romain.giraud@epitech.eu>
2024-10-17 11:55:52 +0000

    [UPDATE] sidebar design sticky

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,9 @@
-<script>
-import SearchBar from "@/components/SearchBar.vue";
-</script>
+<script></script>
 <template>
-  <div class="flex justify-center w-full">
-    <h1 class="text-8xl font-extrabold">Sentrylink</h1>
+  <div class="flex flex-col items-center w-full gap-24 pt-16">
+    <div class="flex justify-center w-full">
+      <h1 class="text-7xl font-extrabold">Sentrylink</h1>
+    </div>
+    <UInput color="blue" size="xl" variant="outline" placeholder="Search..." />
   </div>
 </template>
