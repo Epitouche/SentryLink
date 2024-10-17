86e6f194966fe0235c5e1677d48c0508dd13c9b4
OxiiLB <romain.giraud@epitech.eu>
2024-10-17 16:10:58 +0000

    [UPDATE] Home

---
+++ frontend/components/HomeCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  title: string;
+  subtitle: string;
+  content: string;
+  image: string;
+  isReversed: boolean;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row max-w-screen-xl">
+    <div class="flex flex-row gap-10" v-if="isReversed">
+      <img :src="image" alt="" class="w-1/2 object-cover" />
+      <div class="flex flex-col gap-5 justify-center p-4">
+        <h1 class="text-3xl font-bold">{{ title }}</h1>
+        <h2 class="text-xl opacity-50">{{ subtitle }}</h2>
+        <p>{{ content }}</p>
+      </div>
+    </div>
+    <div class="flex flex-row gap-10" v-else>
+      <div class="flex flex-col gap-5 justify-center w-1/2 p-4">
+        <h1 class="text-3xl font-bold">{{ title }}</h1>
+        <h2 class="text-xl opacity-50">{{ subtitle }}</h2>
+        <p>{{ content }}</p>
+      </div>
+      <img :src="image" alt="" class="w-1/2 object-cover" />
+    </div>
+  </div>
+</template>
--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -4,40 +4,40 @@
   >
     <ul class="flex flex-col items-center h-full">
       <li>
-        <ULink
+        <u-link
           active-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300 black:text-sky-300 text-sky-500"
           inactive-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300"
           to="/"
         >
           <u-icon name="material-symbols:home-rounded" class="w-10 h-10" />
-        </ULink>
+        </u-link>
       </li>
       <li>
-        <ULink
+        <u-link
           active-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300 black:text-sky-300 text-sky-500"
           inactive-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300 "
           to="/dashboard"
         >
           <u-icon name="material-symbols:dashboard-rounded" class="w-10 h-10" />
-        </ULink>
+        </u-link>
       </li>
       <li>
-        <ULink
+        <u-link
           active-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300 black:text-sky-300 text-sky-500"
           inactive-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300"
           to="/settings"
         >
           <u-icon name="material-symbols:settings-rounded" class="w-10 h-10" />
-        </ULink>
+        </u-link>
       </li>
       <li>
-        <ULink
+        <u-link
           active-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300 black:text-sky-300 text-sky-500"
           inactive-class="flex justify-center items-center rounded-md p-2 hover:bg-cprimary-light dark:hover:bg-cprimary transition-colors duration-300"
           to="/profile"
         >
           <u-icon name="material-symbols:person-rounded" class="w-10 h-10" />
-        </ULink>
+        </u-link>
       </li>
     </ul>
   </div>
