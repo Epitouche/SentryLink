85a9d968c294aeaadaf1aaa6a12bebbb578be817
OxiiLB <romain.giraud@epitech.eu>
2024-10-17 16:12:26 +0000

    [UPDATE] Home

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,20 +4,13 @@ import Sidebar from "@/components/Sidebar.vue";
 
 <template>
   <div class="flex min-h-screen">
-    <!-- Conteneur pour gérer le comportement sticky -->
     <div class="relative">
-      <!-- Sidebar sticky à 50% de la vue -->
       <aside class="sticky top-[40vh] p-4">
         <Sidebar />
       </aside>
     </div>
-
-    <!-- Contenu de la page -->
     <main class="flex-1 p-4 overflow-auto">
       <NuxtPage />
-      <!-- Contenu long pour activer le scroll -->
-      <div class="h-[200vh]">
-      </div>
     </main>
   </div>
 </template>
