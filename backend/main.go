c2346959c6e70cdc1c2fcea55df966a59d89c6f5
JsuisSayker <killian.trouve@epitech.eu>
2024-11-06 15:01:38 +0000

    [UPD] swagger docs
    add the swagger documentation off the project on the port 5000

--- backend/main.go
+++ backend/main.go
@@ -10,11 +10,24 @@ import (
 	"github.com/Tom-Mendy/SentryLink/repository"
 	"github.com/Tom-Mendy/SentryLink/service"
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 var db = make(map[string]string)
 
+
 func setupRouter() *gin.Engine {
+
+	docs.SwaggerInfo.Title = "SentryLink API"
+	docs.SwaggerInfo.Description = "SentryLink - Crawler API"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:5000"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+
 	router := gin.Default()
 
 	// Ping test
@@ -50,12 +63,20 @@ func setupRouter() *gin.Engine {
 		}
 
 	}
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	return router
 }
 
+// @securityDefinitions.apiKey bearerAuth
+// @in header
+// @name Authorization
 func main() {
 	router := setupRouter()
+
+
+
+
 	// Listen and Server in 0.0.0.0:8000
 	router.Run(":8000")
 }
