a76e25c75b6fce5cb52f9c9a8c814f9a41e063c8
arthurgauffre <arth.gauffre@gmail.com>
2024-11-22 15:34:25 +0100

    [UPDATE] simplify client.apk.vue: remove unnecessary template markup for cleaner code

--- frontend/pages/client.apk.vue
+++ frontend/pages/client.apk.vue
@@ -1,19 +1,12 @@
-<template>
-    <div>
-      <p>Downloading your APK...</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    mounted() {
-      const apkUrl = '/apk/client.apk';
-  
-      const link = document.createElement('a');
-      link.href = apkUrl;
-      link.download = 'client.apk';
-      link.click();
-    }
-  };
-  </script>
-  
+<script>
+export default {
+  mounted() {
+    const apkUrl = '/apk/client.apk';
+
+    const link = document.createElement('a');
+    link.href = apkUrl;
+    link.download = 'client.apk';
+    link.click();
+  }
+};
+</script>
